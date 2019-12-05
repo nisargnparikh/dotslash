a04816996c82e3802568be3870f2ce06b8002e79
Nisarg Parikh <29868362+nisargnparikh@users.noreply.github.com>
2019-12-04 22:00:32 -0500

    Update gen_inputs.py

--- sample_inputs/gen_inputs.py
+++ sample_inputs/gen_inputs.py
@@ -4,15 +4,10 @@ import subprocess
 import time
 
 subprocess.Popen("> 5-10-15_graphs.txt", shell=True)
-graphs = [5] * 10 + [10] * 10 + [15] * 10 + [20] * 0
+graphs = [5] * 10 + [10] * 10 + [15] * 10 
 for v in graphs:
     subprocess.Popen("./graphGen {0} >> 5-10-15_graphs.txt".format(v), shell=True)
 
-
-subprocess.Popen("> 16v_graphs.txt", shell=True)
-graphs = [16] * 3
-for v in graphs:
-    subprocess.Popen("./graphGen {0} >> 16v_graphs.txt".format(v), shell=True)
 
 subprocess.Popen("> 20v_graph.txt", shell=True)
 graphs = [20] * 1
@@ -20,8 +15,8 @@ for v in graphs:
     subprocess.Popen("./graphGen {0} >> 20v_graph.txt".format(v), shell=True)
 
 
-subprocess.Popen("> 5v_to_2000v_graphs.txt", shell=True)
-graphs = list(range(5,2006,10))
+subprocess.Popen("> 5v_to_1500v_graphs.txt", shell=True)
+graphs = list(range(5,1505,10))
 for v in graphs:
-    subprocess.Popen("./graphGen {0} >> 5v_to_2000v_graphs.txt".format(v), shell=True)
+    subprocess.Popen("./graphGen {0} >> 5v_to_1500v_graphs.txt".format(v), shell=True)
     time.sleep(0.1)

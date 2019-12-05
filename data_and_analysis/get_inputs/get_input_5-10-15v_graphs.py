6057d5605cc7e7380a1eb3b20d346e7ed0107155
Nisarg Parikh <29868362+nisargnparikh@users.noreply.github.com>
2019-12-04 20:49:24 -0500

    Update get_input_5-10-15v_graphs.py

--- data_and_analysis/get_inputs/get_input_5-10-15v_graphs.py
+++ data_and_analysis/get_inputs/get_input_5-10-15v_graphs.py
@@ -7,7 +7,7 @@ for run in range(0,10):
     print('Starting Run: ', run)
     print('================================================================\n\n')
     if run == 0:
-        p = subprocess.Popen("cat ../../sample_inputs/5-10-15_graphs.txt | ../../build/ece650-prj -o -n 5-10-15 -l > 5-15-20_results.txt", shell=True)
+        p = subprocess.Popen("cat ../../sample_inputs/5-10-15_graphs.txt | ../../build/ece650-prj -o -n 5-10-15 -l > 5-10-15_results.txt", shell=True)
     else:
         p = subprocess.Popen("cat ../../sample_inputs/5-10-15_graphs.txt | ../../build/ece650-prj -o -n 5-10-15", shell=True)
     p.wait()
